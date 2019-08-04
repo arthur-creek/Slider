16be0c7a64bb49e8806770ab16cf54891e45a781
Spektra <spektra135@gmail.com>
2019-08-04 18:05:42 +0300

    Version2

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
-var list = document.getElementsByTagName('li')
-for (var i = 0; i<list.length; i++) {
-  list[i].style.position = 'relative';
-  var span = document.createElement('span');
-  span.style.cssText = 'position:absolute; left0; top:0';
-  span.innerHTML = 1;
-  list[i].appendChild(span);
-}
-
+var images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg'];
 var width = 750;
-var count = 1;
-
-var carousel = document.getElementById('carousel');
-var listImages = carousel.querySelector('ul');
-var listElements = carousel.querySelectorAll('li')
-
 var position = 0;
 
+var carousel = document.getElementById('carousel');
+var listImages = carousel.querySelector('ul.images');
+
+var listElements = images.map(function (image) {
+  var li = document.createElement('li');
+  li.classList.add('container-li');
+  listImages.appendChild(li);
+
+  var containerImage = document.createElement('div');
+  containerImage.classList.add('container-image');
+  containerImage.style.backgroundImage = 'url(' + image + ')';
+  li.appendChild(containerImage);
+
+  return li;
+});
+
 carousel.querySelector('.prev').onclick = function() {
-  position = Math.min(position + width * count, 0);
+  console.log('before', position, width);
+  position = Math.min(position + width, 0);
   listImages.style.marginLeft = position + 'px';
+  console.log('after', position);
 };
 
 carousel.querySelector('.next').onclick = function() {
-  position = Math.max(position - width * count, -width*(listElements.length - count));
+  console.log('before', position, width);
+  position = Math.max(position - width, -width * (listElements.length - 1));
   listImages.style.marginLeft = position + 'px';
+  console.log('after', position);
 };
